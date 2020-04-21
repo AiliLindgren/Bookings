69f94e716909938b71741c90bb109440d9c642da
BabyLegs <dinnertimeisnow@gmail.com>
2020-04-21 13:40:51 +0200

    Created Service with a List DB

---
+++ Bookings/Bookings/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookings.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+        public string Contact { get; set; }
+        public int NumberOfVisitors { get; set; }
+        public string StartTime { get; set; } // Datatype?? Double? 
+
+        //public TimeSpan EndTime { get; set; }
+        public string Date { get; set; }
+
+    }
+
+    
+}
---
+++ Bookings/Bookings/Models/ReservationsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookings.Models
+{
+    public class ReservationsService
+    {
+        static List<Reservation> myReservations = new List<Reservation>
+        {
+            new Reservation{ ID = 1, Contact = "Aili", Date = "21.04.2020", NumberOfVisitors = 1, StartTime = "12.00"},
+            new Reservation{ ID = 2, Contact = "Jessica", Date = "22.04.2020", NumberOfVisitors = 1, StartTime = "12.00"},
+            new Reservation{ ID = 3, Contact = "Sointu", Date = "23.04.2020", NumberOfVisitors = 6, StartTime = "12.00"}
+        };
+
+        public Reservation[] GetAll()
+        {
+
+            return myReservations.ToArray();
+        }
+    }
+}
