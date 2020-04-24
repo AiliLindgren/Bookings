891662a38552d1b4d36567d069835307a59ae056
BabyLegs <dinnertimeisnow@gmail.com>
2020-04-24 17:09:08 +0200

    Imported DB till DbProject

---
+++ Bookings/BookingsDB/dbo/Tables/Reservation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Reservation] (
+    [ID]             INT           IDENTITY (1, 1) NOT NULL,
+    [NumberOfPeople] INT           NULL,
+    [StartTime]      DATETIME2 (7) NULL,
+    [EndTime]        DATETIME2 (7) NULL,
+    [Contact]        NVARCHAR (64) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
