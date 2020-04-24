2456cbe593e84200e46a2fb00197cf689cf05c51
AiliLindgren <lindgren.aili@gmail.com>
2020-04-24 10:38:48 +0200

    Same as last one

---
+++ Bookings/SQL Creating new AiliReservationTable.sql
@@ -0,0 +1,9 @@
+﻿create table AiliReservation
+(
+ID int primary key identity not null,
+NumberOfPeople int not null,
+StartTime datetime2 not null,
+EndTime datetime2 not null,
+Contact nvarchar(64) not null
+)
+
