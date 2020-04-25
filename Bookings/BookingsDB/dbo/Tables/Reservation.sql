ab7ba3584617d83f2a688fd98e76e70286fd126d
BabyLegs <dinnertimeisnow@gmail.com>
2020-04-25 11:28:49 +0200

    publish DB with correct var names

--- Bookings/BookingsDB/dbo/Tables/Reservation.sql
+++ Bookings/BookingsDB/dbo/Tables/Reservation.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Reservation] (
     [ID]             INT           IDENTITY (1, 1) NOT NULL,
     [NumberOfPeople] INT           NULL,
-    [StartTime]      DATETIME2 (7) NULL,
-    [EndTime]        DATETIME2 (7) NULL,
+    [StartDateTime]      DATETIME2 (7) NULL,
+    [EndDateTime]        DATETIME2 (7) NULL,
     [Contact]        NVARCHAR (64) NULL,
     PRIMARY KEY CLUSTERED ([ID] ASC)
 );
