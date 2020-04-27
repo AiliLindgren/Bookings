72b591b90b45b836c00f8ac37b98f1a69ebb469a
AiliLindgren <lindgren.aili@gmail.com>
2020-04-27 21:19:22 +0200

    Testing with JS

--- Bookings/Bookings/wwwroot/TestKomponent.js
+++ Bookings/Bookings/wwwroot/TestKomponent.js
@@ -19,3 +19,25 @@
     document.getElementById(totalPeople).innerText = newTotalPeople
 }
 
+function SendDateAndTime() {    
+    var DateAndTime = document.getElementById("ProductID").value
+    document.getElementById("chosenDateAndTime").innerText = DateAndTime
+}
+
+function Tid() {
+    var x = document.getElementById("myBtn").value;
+    document.getElementById("demo").innerHTML = x;
+}
+
+//    once the value from your C# code is stored then you can get it value by javascript
+
+//    var myval = document.getElementById("ProductID").value;
+
+
+//var obj = { name: "John", age: 30, city: "New York" };
+//var myJSON = JSON.stringify(obj);
+//document.getElementById("demo").innerHTML = myJSON;
+
+//function myFunction() {
+//    var x = document.getElementsByTagName("H1")[0].getAttribute("class");
+//    document.getElementById("demo").innerHTML = x;
